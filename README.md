7fed7598b19fd531ddea30a8bec7c12ef182e512
Eslam Hekal <Eslamhekal74@gmail.com>
2022-08-31 15:12:11 +0200

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Front-End-Project
-use javascript to manipulate html web page 
+># Project Name: Landing Page
+
+>## Author Name: Eslam Mohamed Ahmed Hekal    
+
+>## E-mail for support: eslamhekal85@yahoo.com   ;   eslamhekal74@gmail.com
+
+>### Description:
+>> This project aims to convert a completely static webpage into an interactive dynamic one, and we managed to do so by using a number of JavaScript and the DOM tools and that's what we will discuss next briefly.
+>>- first we had to build the navigation bar and to do so we made a use of .querySelectorAll() method to get all the section elements in the webpage which we will put the name of each section of them in the navigation bar, after getting the mentioned info we were able to create a non-ordered list elements to be put inside the navigation bar to point to each section and the methods and properties used for that were:  
+>>  - .createElement()
+>>  - .innerHTML
+>>  - .appendChild()
+>>
+>>
+>>- then we wanted to clarify which section is being viewed while scrolling through the page, so we used .addEventListener() and .getBoundingClientRect() to detect which section is in the viewport while scrolling and after detecting that the event listener will execute tha function (or the listener) take an action to clarify that section.
+>>
+>>
+>>- the last thing in converting our static webpage into a dynamic one is by making the navigation bar elements anchors that when one of them is clicked the page scrolls smoothly to the desired section not only jumps into it and to do that we used an event listener to detect if one of the anchors is clicked then we disabled the property of anchors to jump into the desired place by the method .preventDefault() then by the method scrollIntoView we managed to make it scroll to the desired section when the corresponding anchor is clicked.
+
+> ### How to open and use the project?
+>> All you need to open the webpage are the following files:
+>> - index.html
+>> - app.js
+>> - styles.css   
+>>
+>> so if you have all the mentioned files just double click on the index.html file and it will be opened in your default browser.
---
+++ js/app.js
@@ -0,0 +1,90 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+const sections = document.querySelectorAll('.section');
+const navBar = document.querySelector('#navbar__list');
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+// build the nav
+const fragment = document.createDocumentFragment();
+
+for (let i = 0 ; i< sections.length ; i++)
+{
+  //take section name
+  const liElement = document.createElement('li');
+  const secName = sections[i].querySelector('h2').textContent;
+
+  liElement.innerHTML = `<a href = '#${sections[i].getAttribute('id')}' class = 'navbar__menu menu__Link'> ${secName}</a>`;
+  const anchorElement = liElement.querySelector('a');
+  fragment.appendChild(liElement);
+
+// Add class 'active' to section when near top of viewport
+document.addEventListener('scroll', function(){
+
+const bound = sections[i].getBoundingClientRect();
+if (bound['top'] >= -300 && bound ['top'] < 200) {
+//removing your-active-class class from all sections
+  for(let k = 0; k<sections.length; k++){
+    sections[k].classList.remove('your-active-class');
+    };
+    // Set sections as active
+    sections[i].classList.add('your-active-class');
+    anchorElement.classList.add('your-active-class')
+  }
+  else
+  {
+    sections[i].classList.remove('your-active-class');
+    anchorElement.classList.remove('your-active-class');
+  };
+});
+
+// Scroll to anchor ID using scrollTO event
+liElement.addEventListener('click',function(event){
+  event.preventDefault();
+  // go to section when click on the navList
+  sections[i].scrollIntoView({behavior: 'smooth',block:'center',inline:'nearest'});
+});
+};
+navBar.appendChild(fragment);
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
